d8a469fe6716a7e5dd73c4e8b2121dba18bf1333
Robert Srinivasiah <robbies@gmail.com>
2025-07-30 11:46:49 -0700

    add first test: transforms Only seems to work in certain CLion setups, kinda annoying

---
+++ cpp/src/transform.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2025 Robert Srinivasiah
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("Simple transforms")
+{
+    std::vector<uint32_t> input(100, static_cast<uint32_t>(5));
+
+    std::transform(input.begin(), input.end(), input.begin(), [](const uint32_t i) {
+        return i * 5;
+    });
+
+    CHECK(input.front() == 5 * 5);
+    CHECK(input.back() == 5 * 5);
+}
